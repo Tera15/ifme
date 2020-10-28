4da1ef39e3b743220cb0de2d8366c6d5e4834f2f
cory cathearll <ccatherall15@gmail.com>
2020-10-28 11:15:19 -0400

    ran linter this time

--- client/app/components/Story/__tests__/StoryActions.spec.jsx
+++ client/app/components/Story/__tests__/StoryActions.spec.jsx
@@ -4,27 +4,29 @@ import React from 'react';
 import { StoryActions } from 'components/Story/StoryActions';
 
 describe('storyActions', () => {
-  const { getByText, getByRole, debug } = screen;
+  const { getByText } = screen;
   describe('with create google calendar action', () => {
     it('renders correctly', () => {
-      render(<StoryActions
-        actions={{
-          edit: { link: 'some-url', name: 'Edit' },
-          delete: {
-            link: 'some-url',
-            name: 'Delete',
-            dataMethod: 'delete',
-            dataConfirm: 'Are you sure?',
-          },
-          add_to_google_cal: {
-            link: 'some-url',
-            name: 'Add to google calendar',
-            dataMethod: 'post',
-          },
-          report: { link: 'some-url', name: 'Report' },
-          viewers: 'testViewer',
-        }}
-      />);
+      render(
+        <StoryActions
+          actions={{
+            edit: { link: 'some-url', name: 'Edit' },
+            delete: {
+              link: 'some-url',
+              name: 'Delete',
+              dataMethod: 'delete',
+              dataConfirm: 'Are you sure?',
+            },
+            add_to_google_cal: {
+              link: 'some-url',
+              name: 'Add to google calendar',
+              dataMethod: 'post',
+            },
+            report: { link: 'some-url', name: 'Report' },
+            viewers: 'testViewer',
+          }}
+        />,
+      );
       expect(getByText('Add to google calendar')).toBeInTheDocument();
       expect(getByText('Edit')).toBeInTheDocument();
       expect(getByText('Delete')).toBeInTheDocument();
@@ -34,24 +36,26 @@ describe('storyActions', () => {
   });
   describe('with remove google calendar action', () => {
     it('renders correctly', () => {
-      render(<StoryActions
-        actions={{
-          edit: { link: 'some-url', name: 'Edit' },
-          delete: {
-            link: 'some-url',
-            name: 'Delete',
-            dataMethod: 'delete',
-            dataConfirm: 'Are you sure?',
-          },
-          remove_from_google_cal: {
-            link: 'some-url',
-            name: 'Remove from google calendar',
-            dataMethod: 'delete',
-          },
-          report: { link: 'some-url', name: 'Report' },
-          viewers: 'testViewer',
-        }}
-      />);
+      render(
+        <StoryActions
+          actions={{
+            edit: { link: 'some-url', name: 'Edit' },
+            delete: {
+              link: 'some-url',
+              name: 'Delete',
+              dataMethod: 'delete',
+              dataConfirm: 'Are you sure?',
+            },
+            remove_from_google_cal: {
+              link: 'some-url',
+              name: 'Remove from google calendar',
+              dataMethod: 'delete',
+            },
+            report: { link: 'some-url', name: 'Report' },
+            viewers: 'testViewer',
+          }}
+        />,
+      );
       expect(getByText('Remove from google calendar')).toBeInTheDocument();
       expect(getByText('Edit')).toBeInTheDocument();
       expect(getByText('Delete')).toBeInTheDocument();
